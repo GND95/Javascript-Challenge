64258470d21a644a10c3927fc9d16b92e0ebe5c1
Garrett DeBlois <garrettdeblois13@yahoo.com>
2021-02-21 20:27:14 -0600

    add data from data.js file to webpage. allow user to filter data by date.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,28 @@
-// from data.js
-var tableData = data;
+var tableData = data; // from data.js
+var button = d3.select("#filter-btn"); //select HTML element
+var form = d3.select("#form"); //select HTML element
 
-// YOUR CODE HERE!
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+function runEnter() {
+    d3.event.preventDefault(); // Prevent the page from refreshing
+    var inputElement = d3.select("#datetime"); // Select the input element and get the raw HTML node
+    var inputValue = inputElement.property("value"); // Get the value property of the input element
+    //filter the original array to only include dates which match the date value that the user inputs
+    var filteredData = tableData.filter(UFO => UFO.datetime === inputValue);
+
+    console.log(inputValue);
+    console.log(filteredData);
+
+    var table = d3.select("tbody"); // Select the table element
+    //add data to the table
+    filteredData.forEach((i) => {
+        var row = table.append("tr");
+        Object.entries(i).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+  };
+  
